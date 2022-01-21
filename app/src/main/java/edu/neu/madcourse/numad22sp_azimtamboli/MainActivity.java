a0f138fc31e792c41735d94b019dbcd1991b2fd7
Azim Tamboli <tamboli.a@northeastern.edu>
2022-01-21 17:12:18 -0500

    Hello World

---
+++ app/src/main/java/edu/neu/madcourse/numad22sp_azimtamboli/MainActivity.java
@@ -0,0 +1,35 @@
+package edu.neu.madcourse.numad22sp_azimtamboli;
+
+import static android.app.PendingIntent.getActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void displayAboutMe(View view) {
+
+        Context context = getApplicationContext();
+        CharSequence text = "Name: Azim Tamboli\nEmail: tamboli.a@northeastern.edu";
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+}
