1ee1302a5715c5091050c59aed98f500a2f1244b
Azim Tamboli <tamboli.a@northeastern.edu>
2022-02-05 22:15:24 -0500

    Created new activity and added six buttons

--- app/src/main/java/edu/neu/madcourse/numad22sp_azimtamboli/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_azimtamboli/MainActivity.java
@@ -4,23 +4,34 @@ import static android.app.PendingIntent.getActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+    }
+
+    private void openActivity2() {
+        Intent intent = new Intent(this, NewActivity.class);
+        startActivity(intent);
     }
 
     public void displayAboutMe(View view) {
@@ -28,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         Context context = getApplicationContext();
         CharSequence text = "Name: Azim Tamboli\nEmail: tamboli.a@northeastern.edu";
         int duration = Toast.LENGTH_SHORT;
+
+
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
---
+++ app/src/main/java/edu/neu/madcourse/numad22sp_azimtamboli/NewActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.numad22sp_azimtamboli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+}
